a2c682521d36cc46c87474c2e2370a32985db28a
geniushee <h1116jeon@gmail.com>
2024-06-13 15:31:42 +0900

    git init

---
+++ src/main/java/com/example/redpanda_ex_24_06_13/RedpandaEx240613Application.java
@@ -0,0 +1,13 @@
+package com.example.redpanda_ex_24_06_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedpandaEx240613Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedpandaEx240613Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redpanda_ex_24_06_13/RedpandaEx240613ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redpanda_ex_24_06_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedpandaEx240613ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
